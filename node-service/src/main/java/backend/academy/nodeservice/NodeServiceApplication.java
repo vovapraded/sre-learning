f4f5e37e0220b15fd91da5ff2c996c7e798b2a1f
vladimirpraded <vova.praded@mail.ru>
2025-05-04 20:27:05 +0300

    init

---
+++ node-service/src/main/java/backend/academy/nodeservice/NodeServiceApplication.java
@@ -0,0 +1,25 @@
+package backend.academy.nodeservice;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "NodeService API",
+                version = "v1"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Dev Server")
+        }
+)
+public class NodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeServiceApplication.class, args);
+	}
+
+}
---
+++ node-service/src/test/java/backend/academy/nodeservice/NodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.academy.nodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
